eb861c8a90862f2fcb67f6499c6697214c6fd253
Lazuardi <lazuardi490@gmail.com>
2020-12-04 21:25:07 +0700

    commit ketiga

--- app/src/main/java/lazu/example/youtubeapp/YoutubeActivity.kt
+++ app/src/main/java/lazu/example/youtubeapp/YoutubeActivity.kt
@@ -2,14 +2,21 @@ package lazu.example.youtubeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.RelativeLayout
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.youtube.player.YouTubeBaseActivity
 import com.google.android.youtube.player.YouTubeInitializationResult
 import com.google.android.youtube.player.YouTubePlayer
+import com.google.android.youtube.player.YouTubePlayerView
+
+const val YOUTUBE_VIDEO_ID = "XZXBqpGU8n4"
+const val YOUTUBE_PLAYLIST = "PLFIM0718LjIUqemgG97MAOK0J_berlQM5"
 
 class YoutubeActivity : YouTubeBaseActivity(),YouTubePlayer.OnInitializedListener {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_youtube)
@@ -20,8 +27,15 @@ class YoutubeActivity : YouTubeBaseActivity(),YouTubePlayer.OnInitializedListene
 //        button1.layoutParams = ConstraintLayout.LayoutParams(600, 180)
 //        button1.text = "Button added"
 //        layout.addView(button1)
-    }
 
+        val button1 = YouTubePlayerView(this)
+        button1.layoutParams = RelativeLayout.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+        layout.addView(button1)
+
+        button1.initialize(getString(R.string.GOOGLE_API_KEY), this)
+
+    }
     override fun onInitializationSuccess(
         p0: YouTubePlayer.Provider?,
         p1: YouTubePlayer?,
