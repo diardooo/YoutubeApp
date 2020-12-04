3da47afd9da4f5532c4e4ecf29ea7ce7e61c675c
Lazuardi <lazuardi490@gmail.com>
2020-12-04 13:45:57 +0700

    commit kedua

--- app/src/main/java/lazu/example/youtubeapp/YoutubeActivity.kt
+++ app/src/main/java/lazu/example/youtubeapp/YoutubeActivity.kt
@@ -2,10 +2,38 @@ package lazu.example.youtubeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.RelativeLayout
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.youtube.player.YouTubeBaseActivity
+import com.google.android.youtube.player.YouTubeInitializationResult
+import com.google.android.youtube.player.YouTubePlayer
 
-class YoutubeActivity : AppCompatActivity() {
+class YoutubeActivity : YouTubeBaseActivity(),YouTubePlayer.OnInitializedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_youtube)
+//        setContentView(R.layout.activity_youtube)
+        val layout = layoutInflater.inflate(R.layout.activity_youtube, null)as RelativeLayout
+        setContentView(layout)
+
+//        val button1 = Button(this)
+//        button1.layoutParams = ConstraintLayout.LayoutParams(600, 180)
+//        button1.text = "Button added"
+//        layout.addView(button1)
+    }
+
+    override fun onInitializationSuccess(
+        p0: YouTubePlayer.Provider?,
+        p1: YouTubePlayer?,
+        p2: Boolean
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onInitializationFailure(
+        p0: YouTubePlayer.Provider?,
+        p1: YouTubeInitializationResult?
+    ) {
+        TODO("Not yet implemented")
     }
 }
